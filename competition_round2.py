957c54366160847f20053a4f86f08d321c12be62
Tassadit Sanaa <stass130@gmail.com>
2018-06-09 12:05:20 -0400

    Update competition_round2.py

--- competition_round2.py
+++ competition_round2.py
@@ -42,6 +42,5 @@ def rubricCategory(category_id, participant_id):
         return render_template('rubric3.html', participant = participant, category = category)
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run()
